e6b8f32117d774164c85ba17a405ab8e3cb144ba
Marco-Klopper <110021680+Marco-Klopper@users.noreply.github.com>
2025-10-26 14:54:42 +0200

    Add history command

--- backend/apps/telegram_bot/commands/__init__.py
+++ backend/apps/telegram_bot/commands/__init__.py
@@ -4,3 +4,4 @@ from . import start  # noqa: F401
 from . import register  # noqa: F401
 from . import linkbank  # noqa: F401
 from . import testscore  # noqa: F401
+from . import history  # noqa: F401
